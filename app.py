de799ec8abda9d1d77836e72823dd0dd2c4364c9
Siva Kumar Reddy <gskrscm@users.noreply.github.com>
2020-12-07 15:51:49 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from db import jenkins_db
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
